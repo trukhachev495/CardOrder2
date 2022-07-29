c7ea29f339ad531e7e90b31264d3bc9fbf99fbcb
trukhachev495 <sergey.trainings@gmail.com>
2022-07-29 21:44:53 +0300

    добавил тесты

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -19,6 +19,50 @@ public class CallbackTest {
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+    @Test
+    public void nameFieldValidation(){
+        $("span[data-test-id='name'] input").setValue("Ivan trukchachev");
+        $("span[data-test-id='phone'] input").setValue("+79508556982");
+        $("[data-test-id='agreement']").click();
+        $("[type=button]").click();
+        $("span[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void phoneFieldValidation(){
+        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("span[data-test-id='phone'] input").setValue("+7950855698");
+        $("[data-test-id='agreement']").click();
+        $("[type=button]").click();
+        $("span[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void checkboxValidation(){
+        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("span[data-test-id='phone'] input").setValue("+79508556982");
+        $("[type=button]").click();
+        $("[data-test-id='agreement'].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+    @Test
+    public void notFillingInTheNameField(){
+        $("span[data-test-id='name'] input").setValue("");
+        $("span[data-test-id='phone'] input").setValue("+79508556982");
+        $("[data-test-id='agreement']").click();
+        $("[type=button]").click();
+        $("span[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void notFillingInThePhoneField(){
+        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("span[data-test-id='phone'] input").setValue("");
+        $("[data-test-id='agreement']").click();
+        $("[type=button]").click();
+        $("span[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+
 
 
 }
