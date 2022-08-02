9dce4da6f74425fccc62ef124581f50159421c73
trukhachev495 <sergey.trainings@gmail.com>
2022-08-02 11:40:26 +0300

    исправил ошибки

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -7,62 +7,71 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CallbackTest {
+    
     @BeforeEach
     public void openForm() {
         open("http://localhost:9999/");
     }
+
     @Test
-    public void shouldSubmitRequest(){
-        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
-        $("span[data-test-id='phone'] input").setValue("+79508556982");
-        $("[data-test-id=\"agreement\"]").click();
+    public void shouldSubmitRequest() {
+        $("[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79508556982");
+        $("[data-test-id='agreement']").click();
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
     @Test
-    public void nameFieldValidation(){
-        $("span[data-test-id='name'] input").setValue("Ivan trukchachev");
-        $("span[data-test-id='phone'] input").setValue("+79508556982");
+    public void nameFieldValidation() {
+        $("[data-test-id='name'] input").setValue("Ivan trukchachev");
+        $("[data-test-id='phone'] input").setValue("+79508556982");
         $("[data-test-id='agreement']").click();
         $("[type=button]").click();
-        $("span[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    public void phoneFieldValidation(){
+    public void phoneFieldValidation() {
         $("span[data-test-id='name'] input").setValue("Симонов Андрей");
         $("span[data-test-id='phone'] input").setValue("+7950855698");
         $("[data-test-id='agreement']").click();
         $("[type=button]").click();
-        $("span[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    public void checkboxValidation(){
-        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
-        $("span[data-test-id='phone'] input").setValue("+79508556982");
+    public void checkboxValidation() {
+        $("[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79508556982");
         $("[type=button]").click();
         $("[data-test-id='agreement'].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
+
     @Test
-    public void notFillingInTheNameField(){
-        $("span[data-test-id='name'] input").setValue("");
-        $("span[data-test-id='phone'] input").setValue("+79508556982");
+    public void notFillingInTheNameField() {
+        $("[data-test-id='name'] input").setValue("");
+        $("[data-test-id='phone'] input").setValue("+79508556982");
         $("[data-test-id='agreement']").click();
         $("[type=button]").click();
-        $("span[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    public void notFillingInThePhoneField(){
-        $("span[data-test-id='name'] input").setValue("Симонов Андрей");
-        $("span[data-test-id='phone'] input").setValue("");
+    public void notFillingInThePhoneField() {
+        $("[data-test-id='name'] input").setValue("Симонов Андрей");
+        $("[data-test-id='phone'] input").setValue("");
         $("[data-test-id='agreement']").click();
         $("[type=button]").click();
-        $("span[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
-
-
-
+    @Test
+    public void enteringLastNameWithAHyphen() {
+        $("[data-test-id='name'] input").setValue("Муравьев-Трухачев Сергей");
+        $("[data-test-id='phone'] input").setValue("+79508556982");
+        $("[data-test-id='agreement']").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
